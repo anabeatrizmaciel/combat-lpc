abdc05d2a038a8246c881ee307a57e6b6c95aaec
anabeatrizmaciel <abmn.snf23@uea.edu.br>
2024-02-06 15:11:54 -0400

    Adding 'game' class

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
+# main.py
 import random
-from wall import Wall
-from bullet import Bullet
+from game import Game
 
 def main():
-    battlefield = Wall(20, 10)
-    tank1_x = random.randint(0, battlefield.width // 2)
-    tank1_y = random.randint(0, battlefield.height)
-    tank2_x = random.randint(battlefield.width // 2, battlefield.width - 1)
-    tank2_y = random.randint(0, battlefield.height)
-    bullets = []
+    game_instance = Game()
 
     while True:
-        battlefield.draw_field(tank1_x, tank1_y, tank2_x, tank2_y, bullets)
+        game_instance.battlefield.draw_field(game_instance.tank1_x, game_instance.tank1_y,
+                                             game_instance.tank2_x, game_instance.tank2_y,
+                                             game_instance.bullets)
 
         action1 = input("Player 1 - Choose an action (w, a, s, d to move, t to shoot): ")
-        if action1 == "w" and tank1_y > 0:
-            tank1_y -= 1
-        elif action1 == "s" and tank1_y < battlefield.height - 1:
-            tank1_y += 1
-        elif action1 == "a" and tank1_x > 0:
-            tank1_x -= 1
-        elif action1 == "d" and tank1_x < battlefield.width // 2 - 1:
-            tank1_x += 1
+        action2 = input("Player 2 - Choose an action (w, a, s, d to move, t to shoot): ")
+
+        # Process tank movements and shooting
+        if action1 == "w" and game_instance.tank1_y > 0:
+            game_instance.tank1_y -= 1
+        elif action1 == "s" and game_instance.tank1_y < game_instance.battlefield.height - 1:
+            game_instance.tank1_y += 1
+        elif action1 == "a" and game_instance.tank1_x > 0:
+            game_instance.tank1_x -= 1
+        elif action1 == "d" and game_instance.tank1_x < game_instance.battlefield.width // 2 - 1:
+            game_instance.tank1_x += 1
         elif action1 == "t":
-            new_bullet = Bullet(tank1_x, tank1_y, "d")
-            bullets.append(new_bullet)
+            game_instance.shoot(game_instance.tank1_x, game_instance.tank1_y, "d")
+
+        if action2 == "w" and game_instance.tank2_y > 0:
+            game_instance.tank2_y -= 1
+        elif action2 == "s" and game_instance.tank2_y < game_instance.battlefield.height - 1:
+            game_instance.tank2_y += 1
+        elif action2 == "a" and game_instance.tank2_x > game_instance.battlefield.width // 2:
+            game_instance.tank2_x -= 1
+        elif action2 == "d" and game_instance.tank2_x < game_instance.battlefield.width - 1:
+            game_instance.tank2_x += 1
+        elif action2 == "t":
+            game_instance.shoot(game_instance.tank2_x, game_instance.tank2_y, "a")
 
         # Bullet movement and removal logic
-        bullets = [b for b in bullets if 0 <= b.x < battlefield.width and 0 <= b.y < battlefield.height]
-        for bullet in bullets:
-            bullet.move()
+        game_instance.move_bullets()
 
-        action2 = input("Player 2 - Choose an action (w, a, s, d to move, t to shoot): ")
-        if action2 == "w" and tank2_y > 0:
-            tank2_y -= 1
-        elif action2 == "s" and tank2_y < battlefield.height - 1:
-            tank2_y += 1
-        elif action2 == "a" and tank2_x > battlefield.width // 2:
-            tank2_x -= 1
-        elif action2 == "d" and tank2_x < battlefield.width - 1:
-            tank2_x += 1
-        elif action2 == "t":
-            new_bullet = Bullet(tank2_x, tank2_y, "a")
-            bullets.append(new_bullet)
+        # Check for winner
+        if game_instance.score_tank1 >= 3 or game_instance.score_tank2 >= 3:
+            print("Game Over!")
+            if game_instance.score_tank1 >= 3:
+                print("Player 1 wins!")
+            else:
+                print("Player 2 wins!")
+            break
 
 if __name__ == "__main__":
     main()
+
